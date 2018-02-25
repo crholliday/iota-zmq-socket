ea84bb9646a2e89e066a427c3533993e252cdea9
chris.holliday@gmail.com <chris.holliday@gmail.com>
2018-02-24 21:09:51 -0600

    config update

--- config-template.js
+++ config-template.js
@@ -2,6 +2,6 @@ const path = require('path')
 global.rootPath = path.normalize(path.join(__dirname, '..', '..'))
 
 module.exports = {
-    zmq_url: 'tanglebits.com:5556',
+    zmq_url: 'localhost:5556',
     bind_port: '8091'
 }
